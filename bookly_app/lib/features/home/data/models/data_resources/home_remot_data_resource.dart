76b01e8100de14a81613772b2eadc68d8aad51cc
DESKTOP-MR49FIL\Asus <numan.sh.0101@gmail.com>
2024-01-15 21:00:20 +0200

    - finishing the remote data resources

--- bookly_app/lib/features/home/data/models/data_resources/home_remot_data_resource.dart
+++ bookly_app/lib/features/home/data/models/data_resources/home_remot_data_resource.dart
@@ -1,6 +1,36 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+
+import '../../../../../core/api_service/api_service.dart';
 import '../../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataResource {
-  Future<BookEntity> fetchFeaturedBooks();
-  Future<BookEntity> fetchNewestBooks();
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataResourceImpl implements HomeRemoteDataResource {
+  final ApiService apiServise;
+
+  HomeRemoteDataResourceImpl(this.apiServise);
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiServise.get('volumes?Filtering=free-ebooks&q=medicine');
+    return getBookList(data);
+  }
+
+//volumes?Filtering=free-ebooks&sorting=newest&q=medicine
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiServise
+        .get('volumes?Filtering=free-ebooks&sorting=newest&q=medicine');
+    return getBookList(data);
+  }
+
+  List<BookEntity> getBookList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var element in data['items']) {
+      books.add(BookModel.fromJson(element));
+    }
+    return books;
+  }
 }
