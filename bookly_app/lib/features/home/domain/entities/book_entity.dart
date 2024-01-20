3a255e01a3e6849c261f39f3aa1fb67edd70d1be
DESKTOP-MR49FIL\Asus <numan.sh.0101@gmail.com>
2024-01-20 23:03:43 +0200

    - generating book entity hive adapter

--- bookly_app/lib/features/home/domain/entities/book_entity.dart
+++ bookly_app/lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? imageUrl;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? author;
+  @HiveField(4)
   final double? price;
+  @HiveField(5)
   final int? pageCount;
 
   BookEntity(
