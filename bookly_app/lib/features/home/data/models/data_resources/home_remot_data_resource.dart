0a9beefffb18fce7a5eedcf05d80cfc51cb521fc
DESKTOP-MR49FIL\Asus <numan.sh.0101@gmail.com>
2024-01-20 23:46:28 +0200

    cach featured box

--- bookly_app/lib/features/home/data/models/data_resources/home_remot_data_resource.dart
+++ bookly_app/lib/features/home/data/models/data_resources/home_remot_data_resource.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/core/themes/colors/constants.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:hive/hive.dart';
 
 import '../../../../../core/api_service/api_service.dart';
+import '../../../../../core/functions/save_books.dart';
 import '../../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataResource {
@@ -15,7 +18,9 @@ class HomeRemoteDataResourceImpl implements HomeRemoteDataResource {
   @override
   Future<List<BookEntity>> fetchFeaturedBooks() async {
     var data = await apiServise.get('volumes?Filtering=free-ebooks&q=medicine');
-    return getBookList(data);
+    List<BookEntity> books = getBookList(data);
+    saveBooksData(books, kfeaturedBox);
+    return (books);
   }
 
 //volumes?Filtering=free-ebooks&sorting=newest&q=medicine
@@ -23,7 +28,8 @@ class HomeRemoteDataResourceImpl implements HomeRemoteDataResource {
   Future<List<BookEntity>> fetchNewestBooks() async {
     var data = await apiServise
         .get('volumes?Filtering=free-ebooks&sorting=newest&q=medicine');
-    return getBookList(data);
+    List<BookEntity> books = getBookList(data);
+    return (books);
   }
 
   List<BookEntity> getBookList(Map<String, dynamic> data) {
